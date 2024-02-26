a36014dcd582017d99b3d47fbd4c4c1df9e6e4ee
henrick <hagname01@gmail.com>
2024-02-26 17:32:56 +0100

    branchement serveur smtp

--- src/main/java/com/medHeadReservation/api/controller/ReservationController.java
+++ src/main/java/com/medHeadReservation/api/controller/ReservationController.java
@@ -1,17 +1,22 @@
 package com.medHeadReservation.api.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.medHeadReservation.api.model.Reservation;
 import com.medHeadReservation.api.service.HospitalService;
 import com.medHeadReservation.api.service.ReservationService;
+import com.medHeadReservation.api.service.mail.EmailService;
 
 @RestController
 public class ReservationController {
@@ -20,6 +25,9 @@ public class ReservationController {
     private ReservationService reservationService;
     @Autowired
     private HospitalService HospitalService;
+
+     @Autowired
+    private EmailService emailService;
 
     @PostMapping("/api/web/reservation")
     public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
@@ -30,5 +38,18 @@ public class ReservationController {
     public ResponseEntity<List<Reservation>> getAllReservations() {
         List<Reservation> reservations = reservationService.getAllReservations();
         return ResponseEntity.ok(reservations);
+    }
+     @DeleteMapping("/{id}")
+    public ResponseEntity<?> supprimerReservation(@PathVariable Long id) {
+        // Logique pour supprimer la réservation
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/api/reservations/{id}")
+    public ResponseEntity<Reservation> validerReservation(@PathVariable Long id, @RequestBody Reservation reservation) throws IOException {
+        // Logique pour valider et mettre à jour la réservation
+
+         emailService.sendSimpleMessage(reservation.getMail() , "Confirmation de réservation", "Votre réservation a été traitée et confirmée.");
+        return ResponseEntity.ok(reservation);
     }
 }
